c01f6d6eaaeb4944489891cdd7067a7b4f622325
stcserviciosdiaz <stcserviciosdiaz@outlook.com>
2024-08-25 15:33:14 -0500

    Update

--- sistemaweb/src/app/pages/contactos/contactos.component.ts
+++ sistemaweb/src/app/pages/contactos/contactos.component.ts
@@ -1,93 +1,65 @@
 import { NgIf } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { NavmenuComponent } from '../navmenu/navmenu.component';
 import { FooterComponent } from '../footer/footer.component';
-/* import { FirebaseService } from 'src/app/core/services/firestore.service'; */
+import { FirebaseService } from 'src/app/core/services/firestore.service';
+
+
 
 @Component({
   standalone: true,
   imports: [
     RouterModule,
+    ReactiveFormsModule,
     NgIf,
-    NavmenuComponent, FooterComponent
+    NavmenuComponent,
+    FooterComponent,
   ],
   selector: 'app-contactos',
   templateUrl: './contactos.component.html',
   styleUrls: ['./contactos.component.css']
 })
 export class ContactosComponent implements OnInit {
-
-  contactForm!: FormGroup;
+  contactForm: FormGroup;
   isSubmit = false;
   submitMessage = '';
 
+  constructor(
+    private fb: FormBuilder,
+    private firebaseService: FirebaseService
+  ) {
+    this.contactForm = this.fb.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern('^\\+?[0-9]\\d{1,14}$')]],
+      option: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
 
-constructor(private FormBuilder: FormBuilder) { }
+  ngOnInit(): void {}
 
-/* constructor(private fb: FormBuilder, private firebaseService: FirebaseService) {
-  this.contactForm = this.fb.group({
-    nombre: ['', [Validators.required, Validators.minLength(3)]],
-    email: ['', [Validators.required, Validators.email]],
-    phone: ['', [Validators.required, Validators.pattern(/^[0-9]{10}$/)]],
-    option: ['', Validators.required],
-    message: ['', Validators.required],
-  });
-} */
+  async onSubmit(event: Event) {
+    event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+    this.isSubmit = true;
 
-
-ngOnInit(): void {
-    //this.contactForm = this.initForm();
-}
-
-/* onSubmit(): void{
-  console.log('Form ->', this.contactForm.value);
-
-  this.isSubmit=true;
-  this.contactForm.reset();
-  this.submitMessage = 'Mensaje enviado con exito';
-  setTimeout(()=>{
-    this.isSubmit=false;
-  },8000);
-
-} */
-
-async onSubmit() {
-  console.log('entro')
-  //this.isSubmit = true;
-
-
-
-  if (this.contactForm.valid) {
-    try {
-      //await this.firebaseService.saveData(this.contactForm.value);
-      console.log('Mensaje enviado con éxito', this.contactForm.value)
-      this.submitMessage = 'Mensaje enviado con éxito';
-    } catch (error) {
-      this.submitMessage = 'Error al enviar el mensaje';
+    if (this.contactForm.valid) {
+      try {
+        const formData = this.contactForm.value;
+        await this.firebaseService.saveContactForm(formData);
+        this.submitMessage = 'Mensaje enviado con éxito';
+        this.contactForm.reset(); // Opcional: resetea el formulario después de enviarlo
+      } catch (error) {
+        this.submitMessage = 'Error al enviar el mensaje';
+      }
+    } else {
+      this.submitMessage = 'Por favor, completa todos los campos requeridos.';
     }
-  } else {
-    this.submitMessage = 'Por favor, completa todos los campos requeridos.';
   }
 }
 
-/* initForm (): FormGroup{
-  return this.fb.group({
-    nombre: ['', [Validators.required, Validators.minLength(4)]],
-    email: ['', [Validators.required]],
-    phone: [''],
-    option: [''],
-    message: [''],
-  })
-
-} */
-
-
-
-
-
-}
 
 
 
